b159dbaa9691dc9450e0503aff5e688f715ef6f1
MungkornFai <phaiyladsamee@gmail.com>
2023-10-30 17:33:48 +0700

    complete ui dashboard

---
+++ app/ui/fonts.ts
@@ -0,0 +1,17 @@
+import { Inter, Lusitana, Roboto, Noto_Sans_Lao } from 'next/font/google';
+
+export const inter = Roboto({
+  weight: ['500'],
+  subsets: ['latin'],
+});
+
+
+export const lusitana = Lusitana({
+    weight: ['400'],
+    subsets: ['latin'],
+});
+
+export const lao_font = Noto_Sans_Lao({
+    weight: ['500'],
+    subsets: ['latin'],
+});
